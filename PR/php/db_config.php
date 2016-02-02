f2734df38a615bb541e72202e6be9b361571283b
haojunzhang <rin841005@gmail.com>
2016-02-02 17:38:54 +0800

    2-2
    1.remove php/doc

--- PR/php/db_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_USER', "User_Program"); // db user
-define('DB_PASSWORD', "ru8 vu/ nj4ej03"); // db password (mention your db password here)
-define('DB_DATABASE', "gbdormitory"); // database name
-define('DB_SERVER', "127.0.0.1"); // db server
-?>
