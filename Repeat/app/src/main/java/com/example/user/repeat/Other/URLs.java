c4720ad096332367269db5dc730f064647801dee
haojunzhang <rin841005@gmail.com>
2016-02-02 17:34:52 +0800

    2-2
    1.new select score/putt method
    2.history bug
    3.export ok

--- Repeat/app/src/main/java/com/example/user/repeat/Other/URLs.java
+++ Repeat/app/src/main/java/com/example/user/repeat/Other/URLs.java
@@ -4,11 +4,12 @@ package com.example.user.repeat.Other;
  * Created by v on 2015/12/12.
  */
 public class URLs {
-    private static final String serverip = "http://140.131.115.47/php/";
-    public static final String url_login = serverip + "pb_login.php";
+    private static final String serverip = "http://140.131.115.40:8000/gbDormitory/AppProcess/";
+    public static final String url_login = serverip + "pb_login.jsp";
     public static final String url_allannouncement = serverip + "pb_allannouncement.php";
     public static final String url_allproblem = serverip + "pb_allproblem.php";
     public static final String url_addproblem = serverip + "pb_addproblem.php";
     public static final String url_updatestart = serverip + "pb_updatestart.php";
     public static final String url_gcm_register = serverip + "gcm_register.php";
+     
 }
